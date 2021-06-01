362b7fa43814ca3bb52fde84690a7abbd893d681
raiymbekkabiyev <72629885+raiymbekkabiyev@users.noreply.github.com>
2021-06-01 15:49:35 +0600

    testing screenup and screen down switch

--- sketch_may31b/sketch_may31b.ino
+++ sketch_may31b/sketch_may31b.ino
@@ -11,7 +11,6 @@
 //vars
 bool IMU6886Flag = false;
 uint8_t FSM = 0;
-float tempC = 0;
 CRGB ListOfColors[] = {0x000000, 0x00ff00, 0xffffff}; //black, red, white
 float tempInC = 0; //Temperature in Celcius
 float tempInF = 0; //Temperature in Farengate
@@ -19,26 +18,6 @@ float tempInF = 0; //Temperature in Farengate
 float accX = 0;
 float accY = 0;
 float accZ = 0;
-
-//To make it clear that the screen is activated, I want to draw a circle
-int circleWithX[25] =
-{
-  0, 1, 1, 1, 0,
-  1, 0, 2, 0, 1,
-  1, 2, 2, 2, 1,
-  1, 0, 2, 0, 1,
-  0, 1, 1, 1, 0
-};
-
-//Functions
-//this function will be used to display circle on the screen
-void drawMatrix(int mat[], int color1)
-{
-  for (int i = 0; i < 25; i++)
-  {
-    M5.dis.drawpix(i, color1);
-  }
-}
 
 //Inspired by Mike Klepper's article I will use Accelerometer to see if screen is facing upwards
 /*
@@ -49,9 +28,7 @@ void drawMatrix(int mat[], int color1)
 float LOW_TOL = 100;
 float HIGH_TOL = 900;
 
-float scaledAccX = 0;
-float scaledAccY = 0;
-float scaledAccZ = 0;
+float scaledAccX = 0, scaledAccY = 0, scaledAccZ = 0;
 
 
 
@@ -75,30 +52,35 @@ void loop() {
     //Temperature is being measured constanly on the background
     M5.IMU.getTempData(&tempInC);
     Serial.printf(" Temp : %.2f C \r\n", tempInC);
-    float tempInF = tempC * 9 / 5 + 32;
+    float tempInF = tempInC * 9 / 5 + 32;
     Serial.printf(" Temp : %.2f F \r\n", tempInF);
 
-    //Taking input from Accelerometer 
+    //Taking input from Accelerometer to identify orientaion of the devise
     M5.IMU.getAccelData(&accX, &accY, &accZ);
     Serial.printf("Accel: %.2f, %.2f, %.2f mg\r\n", accX * 1000, accY * 1000, accZ * 1000);
     scaledAccX = accX * 1000;
     scaledAccY = accY * 1000;
     scaledAccZ = accZ * 1000;
-    
+
     //Here we need to Activate the screen and conditions are facing it upwards and pressing a btn
     if (abs(scaledAccX) < LOW_TOL && abs(scaledAccY) < LOW_TOL && abs(scaledAccZ) > HIGH_TOL && scaledAccZ < 0)
     {
-      drawMatrix(circleWithX, 0x00ff00);
+      if (M5.Btn.wasPressed()) {
+        M5.dis.fillpix(0xff0000); //fill screen green to signify that devise was activated
+      }
+
     }
-    else {
-      break; 
+    //this is sleep mode
+    else if (abs(scaledAccX) < LOW_TOL && abs(scaledAccY) < LOW_TOL && abs(scaledAccZ) > HIGH_TOL && scaledAccZ > 0) {
+      //switch off LED-s
+      M5.dis.clear();
+      //Keep reading temperature data
+      M5.IMU.getTempData(&tempInC);
+      Serial.printf(" Temp : %.2f C \r\n", tempInC);
+      float tempInF = tempInC * 9 / 5 + 32;
+      Serial.printf(" Temp : %.2f F \r\n", tempInF);
+      
     }
-
-
-
-
-
-
 
 
 
