91ac69e2e777c291ab8d1d71b384caf62f8d2310
MayhemKM <73580356+MayhemKM@users.noreply.github.com>
2021-06-02 17:11:27 +0400

    Update RecordTemp.ino

--- RecordTemp.ino
+++ RecordTemp.ino
@@ -1,74 +1,79 @@
 #include "M5Atom.h"
 #define INTERVAL 5000; //Interval to record temperature (milliseconds)
 
+unsigned long targetTime (0);
+float every5s [60], every1m [60], every1h [60], every1d [60], mAvg, hAvg, dAvg(999.9), temp(999.99);
+int sCtr (0), mCtr (0), hCtr (0);
+
+
+void shiftFloatArray(float myArray[], float firstValue) {
+  for (int ctr = 59; ctr > 0; ctr--)
+  myArray[ctr] = myArray[ctr - 1];
+  myArray[0] = firstValue;
+}
+
+float average(float array[60], int averageOf) {
+  float tempSum (0);
+  for (int ctr = 0; ctr < averageOf; ctr++)
+    tempSum += array[ctr];
+  return (tempSum / (float)averageOf);
+}
+
 void setup() {
   M5.begin(true, false, true);
   M5.IMU.Init();
+
+  for (int ctr = 0; ctr < 60; ctr++)
+    every5s[ctr] = every1m[ctr] = every1h[ctr] = every1d[ctr] = 999.99;
 }
 
-unsigned long targetTime (0);
-float every5s [60], every1m [60], every1h [60], every1d [60];
-int sCtr (0), mCtr (0), hCtr (0), dCtr (0);
-float temp (0), tempSum (0);
 
 void loop() {
-  if ( millis() > targetTime) {
-    M5.IMU.getTempData(&temp);
-    every5s[sCtr] = temp;
+  if ( millis() > targetTime) {  //If it's time to record the temperature
+    M5.IMU.getTempData(&temp);  //Get the current temperature from the device and store it in the variable temp
+
+    shiftFloatArray(every5s, temp);
     sCtr++;
-    if (sCtr == 12 || sCtr == 24 || sCtr == 36 || sCtr == 48 || sCtr == 60) {//##use remainder modulus %
-      tempSum = 0;
-      for (int ctr = 1; ctr < 13; ctr++) {
-        tempSum += every5s[sCtr - ctr];
-      }
-      every1m[mCtr] = tempSum / 12.0;
+
+    if (sCtr == 12)  {  //If it has been a minute
+      mAvg = average(every5s, 12);
+      shiftFloatArray(every1m, mAvg);
       mCtr++;
-      if (mCtr > 59) {
-        tempSum = 0;
-        for (int ctr = 0; ctr < 60; ctr++) {
-          tempSum += every1m[mCtr - ctr];
-        }
-        every1h[hCtr] = tempSum / 60.0;
+
+      if (mCtr == 60) {  //If it has been an hour
+        hAvg = average(every1m, 60);
+        shiftFloatArray(every1h, hAvg);
         hCtr++;
-        if (hCtr > 59) {
-          for (int ctr = 0; ctr < 60; ctr++) {
-            tempSum += every1h[hCtr - ctr];
-          }
-          if (dCtr < 23) {
-            every1d[dCtr] = tempSum / 24.0;
-            dCtr++;
-          }
-          else {
-            for (int ctr = 0; ctr < 59; ctr++) {
-              every1d[ctr + 1] = every1d[ctr];
-            }
-            every1d[0] = tempSum / 24.0;
-          }
+
+        if (hCtr == 24) {  //If it has been a day
+          dAvg = average(every1h, 24);
+          shiftFloatArray(every1d, dAvg);
+          hCtr = 0;
         }
+
+        mCtr = 0;
       }
-      if (sCtr > 59)
-        sCtr = 0;
+
+      sCtr = 0;
     }
-    targetTime += INTERVAL;
-    //
+
+    targetTime += INTERVAL;  //Set the new target time to one interval away from the last target time
+
+    //Used to check data storage on serial monitor
     Serial.print("\nData of every 5 secs for last 5 mins:  ");
-    for (int ctr = 0; ctr < 60; ctr++) {
+    for (int ctr = 0; ctr < 60; ctr++)
       Serial.printf("%.2f,  ", every5s [ctr]);
-    }
     Serial.print("\nAverage temp data of every 1 min for last 60 mins:  ");
-    for (int ctr = 0; ctr < 60; ctr++) {
+    for (int ctr = 0; ctr < 60; ctr++)
       Serial.printf("%.2f,  ", every1m [ctr]);
-    }
     Serial.print("\nAverage temp data of every 1 hr for last 60 hrs:  ");
-    for (int ctr = 0; ctr < 60; ctr++) {
+    for (int ctr = 0; ctr < 60; ctr++)
       Serial.printf("%.2f,  ", every1h [ctr]);
-    }
     Serial.print("\nAverage temp data of every 1 day for last 60 days:  ");
-    for (int ctr = 0; ctr < 60; ctr++) {
+    for (int ctr = 0; ctr < 60; ctr++)
       Serial.printf("%.2f,  ", every1d [ctr]);
-    }
     Serial.println();
-    //
-  }
+
+  } //End of what happens if it's time to record the temperature
   M5.update();
 }
